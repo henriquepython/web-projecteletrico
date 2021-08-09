6670974da6f7544867815c9b534f094606b98023
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-08-09 16:10:59 -0300

    Update script

--- script.js
+++ script.js
@@ -26,41 +26,41 @@ var perimetrobanheiro = document.getElementById("pbanheiros");
 
 const tugs = (x) => {
     var tomada = math.trunc(x / 5);
-    return tomada.values//virar arrow function
+    return tomada.value//virar arrow function
 }
 const lumins = (y) => {
     var luminarias = math.trunc(y / 5);
-    return luminarias.values//virar arrow function
+    return luminarias.value//virar arrow function
 } 
 
 
 //final calculos comodos quarto
-var tugquarto = (tugs(perimetroquarto.values))*qquarto.values; //mostrar no layout
-var lumiquarto = (lumins(areaquarto.values))*qquarto.values; //mostrar no layout
+var tugquarto = (tugs(perimetroquarto.value))*qquarto.value; //mostrar no layout
+var lumiquarto = (lumins(areaquarto.value))*qquarto.value; //mostrar no layout
 var potenciaquarto = ((tugquarto * 100) + (lumiquarto * 60));
-var correntesquarto = potenciaquarto1 / tensao;
+var correntesquarto = potenciaquarto1 / tensao.value;
 //final calculos comodos sala
-var tugsala = (tugs(perimetrosala.values))*qsala.values; //mostrar no layout
-var lumisala = (lumins(areasala.values))*qsala.values; //mostrar no layout
+var tugsala = (tugs(perimetrosala.value))*qsala.value; //mostrar no layout
+var lumisala = (lumins(areasala.value))*qsala.value; //mostrar no layout
 var potenciasala = ((tugsala * 100) + (lumisala1 * 60));
-var correntessala = potenciasala / tensao;
+var correntessala = potenciasala / tensao.value;
 //final calculos comodos cozinha
-var tugcozinha = (tugs(perimetrocozinha.values))*qcozinha.values; //mostrar no layout
-var lumicozinha = (lumins(areacozinha.values))*qcozinha.values; //mostrar no layout
+var tugcozinha = (tugs(perimetrocozinha.value))*qcozinha.value; //mostrar no layout
+var lumicozinha = (lumins(areacozinha.value))*qcozinha.value; //mostrar no layout
 var potenciacozinha = ((tugcozinha * 100) + (lumicozinha * 60));
-var correntescozinha = potenciacozinha / tensao;
+var correntescozinha = potenciacozinha / tensao.value;
 //final calculos comodos banheiro
-var tugbanheiro = (tugs(perimetrobanheiro.values))*qbanheiro.values; //mostrar no layout
-var lumibanheiro = (lumins(areabanheiro.values))*qbanheiro.values; //mostrar no layout
+var tugbanheiro = (tugs(perimetrobanheiro.value))*qbanheiro.value; //mostrar no layout
+var lumibanheiro = (lumins(areabanheiro.value))*qbanheiro.value; //mostrar no layout
 var potenciabanheiro = ((tugbanheiro * 100) + (lumibanheiro * 60));
-var correntesbanheiro = potenciabanheiro / tensao;
+var correntesbanheiro = potenciabanheiro / tensao.value;
 
 //total
 var pottotal = potenciaquarto + potenciasala + potenciacozinha + potenciabanheiro + potenciaexclusivo;
 
 //Padrão de entrada
 //FINAL
-var correntedeprojeto = pottotal / tensao;
+var correntedeprojeto = pottotal / tensao.value;
 
 a = 0
 
